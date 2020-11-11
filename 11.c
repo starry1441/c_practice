66d08ec3173b4979570e8e58e81a77b61c702518
starry1441 <3363654551@qq.com>
2020-11-11 16:43:30 +0800

    一些关于c语言函数调用的练习。

---
+++ 11.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+////实现函数判断year是不是润年。
+//
+//void IsLeapYear(int n)
+//{
+//	if (n % 4 == 0 && n % 100 != 0 || n % 400 == 0)
+//	{
+//		printf("是闰年\n");
+//	}
+//	else
+//	{
+//		printf("不是闰年\n");
+//	}
+//}
+//
+//int main()
+//{
+//	int a;	//年份
+//	printf("请输入年份\n");
+//	scanf("%d", &a);
+//	IsLeapYear(a);
+//}
---
+++ 12.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+////实现一个函数来交换两个整数的内容。
+//
+//void ChangeNum(int *a, int *b)
+//{
+//	int c = 0;
+//	c = *a;
+//	*a = *b;
+//	*b = c;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	printf("请输入两个整数\n");
+//	scanf("%d%d", &a, &b);
+//	ChangeNum(&a, &b);
+//	printf("%d %d", a, b);
+//}
